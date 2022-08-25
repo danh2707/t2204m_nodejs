a3bcfc7a69c53cc640983de9d2e179ef9d85eace
danh2707 <105687037+danh2707@users.noreply.github.com>
2022-08-25 09:23:04 +0700

    up code

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log("Hello world");
+// var x=10;
+// var y=20;
+// console.log(x+y);
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT | 5000;
+
+app.listen(port,function () {
+    console.log("Server is running...");
+
+})
+app.get("/",function (req,res) {
+    res.send("Hello world");
+})
+app.get("/bong-da",function (req,res) {
+    res.send("Bóng đá 24h");
+})
+
